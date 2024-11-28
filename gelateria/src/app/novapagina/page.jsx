130350c64db9a9948c3f0cdd3209a46fa4c275d9
Guilherme <guilherm.rodrigues@outlook.com>
2024-11-28 08:01:29 -0300

    segundo commit

---
+++ gelateria/src/app/novapagina/page.jsx
@@ -0,0 +1,11 @@
+export default function NovaPagina() {
+    return (
+      <main>
+        <div>
+          <h1>Nova Página</h1>
+          <p>Essa é a nova página da aplicação.</p>
+          <p>Essa é a nossa primeira página criada no React.</p>
+        </div>
+      </main>
+    );
+  }
