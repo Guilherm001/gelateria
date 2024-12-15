8805bdca4736bcdf091e7eac839d9e76ff684160
Guilherme <147566218+Guilherm001@users.noreply.github.com>
2024-12-15 11:10:16 -0300

    commit refente ao projeto da Camila

---
+++ gelateria/src/componentes/outro-projeto.module.jsx
@@ -0,0 +1,18 @@
+
+
+export default function (){
+
+    <h1 className={teste}> " " </h1>;
+
+    let compra = ValorCompra;
+    let venda = ValorVenda;
+    let porcentagem = CalculoPorcentagem;
+
+    return(
+         function calculculo (){
+            return(
+                porcentagem = compra / venda
+            )
+         }
+    )
+}
